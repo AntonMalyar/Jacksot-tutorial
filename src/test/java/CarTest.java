97f332705126959ae0ea755f819722f13012c5a4
Anton Malyar <antn.mlr@gmail.com>
2022-02-22 12:18:07 +0200

    create test - deserialize Java Object Car From JSON string

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,3 +1,18 @@
+import ObjectMapper.practice.Car;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CarTest {
+    @Test
+    void deserializeJavaObjectsFromJSON() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String carJson = "{ \"brand\" : \"Mercedes\", \"doors\" : 5 }";
+        Car car = objectMapper.readValue(carJson, Car.class);
+
+        Assertions.assertEquals("Mercedes", car.brand);
+        Assertions.assertEquals(5, car.doors);
+    }
 
 }
